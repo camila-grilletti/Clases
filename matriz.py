b38353f9095c8f67a97f1e36b77cfb40720bb117
camila-grilletti <c.grilletti@alumno.etec.um.edu.ar>
2021-09-09 19:54:38 -0300

    #2

--- matriz.py
+++ matriz.py
@@ -6,18 +6,18 @@ class Matriz:
         self.matriz3 = []
     def ingresar(self):
         for i in self.elementos:
-            n=int(input("Ingrese un valor 1: "))
+            n=int(input("Ingrese elementos para la primera matriz: "))
             self.matriz1.append(n)
         for j in self.elementos:
-            m=int(input("Ingrese un valor 2: "))
+            m=int(input("Ingrese elementos para la segunda matriz: "))
             self.matriz2.append(n)
-        print(self.matriz1)
-        print(self.matriz2)
+        print("Matriz 1: ", self.matriz1)
+        print("Matriz 2: ", self.matriz2)
     def sumaMatrices(self):
         for i in range(len(self.matriz1)):
             self.matriz3.append(self.matriz1[i]+self.matriz2[i])
-        return self.matriz3
+        return self.matriz3   
 
 matriz = Matriz()
 matriz.ingresar()
-print(matriz.sumaMatrices())
+print("Suma de matrices: ", matriz.sumaMatrices())
